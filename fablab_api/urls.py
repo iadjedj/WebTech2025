702ef992c823733116bcbf2b5813c8760bdbf10f
Sonia boutaleb <sonia.boutaleb@next-u.fr>
2025-01-29 12:32:34 +0100

    route verif poids pour output scan

--- fablab_api/urls.py
+++ fablab_api/urls.py
@@ -1,8 +1,13 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from commandes.views import ProduitViewSet, SandwichViewSet, CommandeViewSet, stock_actuel  # Assure-toi que l'import est correct
+from commandes.views import (
+    ProduitViewSet, 
+    SandwichViewSet, 
+    CommandeViewSet, 
+    stock_actuel, 
+    verifier_poids_commande  # 🔹 Ajout de l'import pour la vérification du poids
+)
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -17,7 +22,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),  # Inclut toutes les routes API
     path('api/stock/', stock_actuel, name='stock'),  # Ajout de l'endpoint pour le stock
+    path('api/verification-poids/', verifier_poids_commande, name="verification-poids"),  # 🔹 Nouveau endpoint pour la vérification du poids
 ]
+
 # Ajoute cette ligne pour servir les fichiers statiques en mode DEBUG
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
